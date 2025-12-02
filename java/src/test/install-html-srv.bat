fd6387555509030626e4bde8c6d4114519a3b1ca
Odys Zhou <odys.zhou@gmail.com>
2025-12-02 23:33:35 +0800

    0.7.7 ISSUE / NOTES / MEMO:
    Pack JRE for Windows & Install Win-Serivce
    ==========================================
    
    To install a packaged & installed jre for to be used by Apache Common Daemon (procrun),
    
      --jvm=jre-path\bin\server\jvm.dll
    
    Grok insist that it should be 'bin\java.dll', and must be compiled as a flat dll. Such
    jdk/jre image cannot be found both at Temurin (Github) and Zulu(https://www.azul.com/downloads/?package=jdk#zulu)
    
    Fix JRE downloading problems
    ============================
    
    - Rename the Distribution to ...x64-windows-[org-id].zip
    
      or ...-online-[org-id].zip if ignored jre-release in tasks.json.
    
    - distable test button in GUI
    
    - verfiy central pswd with Registration/jserv/tasks.json
    
    Tasks / TODOs / Wishes
    ======================
    
    - Semantically synchronize a_users (and a_orgs?), for console manangement built on edge infrastructure?
    
    - Let the users plan the domain, at Central?
    
    - The efficent/effective way of personal IMS?
    
    - Implement Nyquence

--- java/src/test/install-html-srv.bat
+++ java/src/test/install-html-srv.bat
@@ -4,17 +4,29 @@
 @echo Usage: src/test/install-html-srv.bat
 @echo   [1]prunsrv(e.g. src\test\prunsrv.exe) [2]jar-name [3]service-name
 @echo   [4]resource-path(or web-root, e.g. src\main\webapp) [5]main-class(HtmlServer) [6]main-package(io.oz.srv.main-class)
-
+@echo   [7]jvm option, the jre path(optional, e.g. jre17), relative to the workfolder. If not given, use 'auto'.
 
 @set prunsrv=%1
+@echo [1 prunsrv]      = %1
 @set jar=%2
+@echo [2 jar]          = %2
 @set servic_name=%3
+@echo [3 service-name] = %3
 @set res_path=%4
+@echo [4 res-path]     = %4
 @set mainclass=%5
+@echo [5 main-class]   = %5
 @set serv_class=%6
+@echo [6 jserv-class]  = %6
 @set jre_path=%7
+@echo [7 jre-path]     = %7
 
 @set workfolder=%cd%
+
+if not "%~7" == "" (
+@set jre_path=%workfolder%\%7\bin\server\jvm.dll --JavaHome=%workfolder%\%7 
+) else (@set jre_path=auto)
+
 @set classpath=%workfolder%\%jar%
 
 @echo Jar package: %classpath%
@@ -23,13 +35,13 @@
 
 @echo "Finding service main class:"
 jar tf %classpath% | findstr "%mainclass%"
+@echo jvm=%jre_path% # jre17/bin/server/jvm.dll
 
 @echo:
 @echo ACTION NEEDED!
 @echo:
 @echo Please confirm permission (in the hidden dialog) to install the service %servic_name%...
 
-@echo jvm=%jre_path% # jre17/bin/jvm.dll
 
 @%prunsrv% //IS//%servic_name% --Install=%workfolder%\%prunsrv% ^
 --ServiceUser LocalSystem ^
